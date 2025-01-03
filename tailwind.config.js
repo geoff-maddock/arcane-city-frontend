9b78c81dd1aac25effe095fff116f116d8bef2ca
Geoff Maddock <geoff.maddock@gmail.com>
2025-01-03 01:18:57 -0500

    Fixed config for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+import animate from "tailwindcss-animate"
+
+export default {
 	darkMode: ["class"],
 	content: [
 		"./index.html",
@@ -70,5 +72,5 @@ module.exports = {
 			},
 		},
 	},
-	plugins: [require("tailwindcss-animate")],
+	plugins: [animate],
 }
