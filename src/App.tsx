a869c22d3acd3bc8cf821bf53eba9e676b487712
Geoff Maddock <geoff.maddock@gmail.com>
2025-01-06 19:22:33 -0500

    Fixed root route and menu.

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,15 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from '@tanstack/react-router';
 import { router } from './router';
-import MenuBar from './components/MenuBar';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router}>
-        <div className="flex">
-          <MenuBar />
-          <div className="flex-1">
-            {/* Other components that need routing */}
-          </div>
-        </div>
-        <ReactQueryDevtools initialIsOpen={false} />
-        <Analytics />
-      </RouterProvider>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Analytics />
     </QueryClientProvider>
   );
 }
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,6 +1,17 @@
-// src/routes/root.tsx
 import { createRootRoute, Outlet } from '@tanstack/react-router';
+import MenuBar from '../components/MenuBar';
+
+export const Root = () => {
+    return (
+        <div className="flex">
+            <MenuBar />
+            <div className="flex-1">
+                <Outlet />
+            </div>
+        </div>
+    );
+};
 
 export const rootRoute = createRootRoute({
-    component: () => <Outlet />
+    component: Root,
 });
