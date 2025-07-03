da89c9a2fe06adf2f05d7c3fa8d8949cb2d3f37f
Geoff Maddock <geoff.maddock@gmail.com>
2025-07-03 02:29:19 -0400

    Removed unused sort controls import

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -3,7 +3,6 @@ import { useEvents } from '../hooks/useEvents';
 import EventCard from './EventCard';
 import EventFilter from './EventFilters';
 import { Pagination } from './Pagination';
-import SortControls from './SortControls';
 import { Loader2 } from 'lucide-react';
 import { Card, CardContent } from '@/components/ui/card';
 import { Alert, AlertDescription } from '@/components/ui/alert';
